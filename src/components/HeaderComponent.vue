8d3946d3586580079dbdb7a276db8df6328ea04e
LorenzoVeropalumbo <lorenzo-veropalumbo@outlook.it>
2022-07-15 11:26:06 +0200

    scafhold base

---
+++ src/components/HeaderComponent.vue
@@ -0,0 +1,15 @@
+<template>
+  <header>
+    <div class="header-top"></div>
+  </header>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style lang="scss" scoped>
+  @import "../style/variables.scss";
+</style>
